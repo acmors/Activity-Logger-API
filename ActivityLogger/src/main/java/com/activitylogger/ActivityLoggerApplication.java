d926915a304a61a5aa2daf9a5a93a101a2631526
acmors <marcosantoniosilva047@gmail.com>
2024-11-30 10:03:30 -0300

    first commit

---
+++ ActivityLogger/src/main/java/com/activitylogger/ActivityLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.activitylogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivityLoggerApplication.class, args);
+	}
+
+}
---
+++ ActivityLogger/src/test/java/com/activitylogger/ActivityLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitylogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
